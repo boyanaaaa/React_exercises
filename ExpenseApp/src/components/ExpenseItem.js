7c18972510374ef45880b2c9c9f3eccdf8706165
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-04-05 12:21:09 +0300

    Update ExpenseItem.js

--- ExpenseApp/src/components/ExpenseItem.js
+++ ExpenseApp/src/components/ExpenseItem.js
@@ -1,18 +1,24 @@
+import React from 'react';
 import './ExpenseItem.css';
+import Card from './Cards';
 
-function ExpenseItem(props) {
+
+
+const  ExpenseItem = (props) => {
     //const month = props.date.toLocaleString('en-US', {month: 'long'});
     //const day = props.date.toLocaleString('en-US', {day: '2-digit'});
     //const year = props.date.getFullyear();
     return (
-     <div className = 'expense-item'>
+        <Card classname='espense-item'>
+            <div className = 'expense-item'>
         
-        <div className = 'expense-item__description'>
-            <h2>{props.title}</h2>
-        </div>
-        <div className = 'expense-item__price'>${props.price}</div>
-     </div>   
-    )
+                <div className = 'expense-item__description'>
+                <h2>{props.title}</h2>
+                </div>
+                <div className = 'expense-item__price'>${props.amount}</div>
+            </div>  
+     </Card> 
+    );
 }
 
 export default ExpenseItem;
