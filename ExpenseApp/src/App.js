ffa69b45759e33dffebebbb802da11b0585d2efb
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-04-07 11:46:42 +0300

    Update App.js

--- ExpenseApp/src/App.js
+++ ExpenseApp/src/App.js
@@ -1,36 +1,48 @@
 import React from 'react';
-import './App.css';
-import Expenses from './components/Expenses';
 
-const  App = () => {
+import NewExpense from './components/NewExpense/NewExpense';
+import Expenses from './components/Expenses/Expenses';
+
+const App = () => {
   const expenses = [
     {
-      id: 'el',
+      id: 'e1',
       title: 'Toilet Paper',
-      amount: 94.29,
-      /* date: new Date(2020, 4, 24), */
-    
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
     },
     {
-      id: 'el',
-      title: 'car Insurance',
-      amount: 294.29,
-      
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
     },
-    {
-      id: 'el',
-      title: 'Brrrr',
-      amount: 984.29,
-      
-    }
-  ]
+  ];
+
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
 
   return (
     <div>
-      <h2> Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
-    
   );
 }
 
