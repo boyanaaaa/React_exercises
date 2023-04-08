dc363303a6f7014486255c5e46b5291df7d31713
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-04-08 23:00:47 +0300

    Create App.js

---
+++ Travel_website/App.js
@@ -0,0 +1,17 @@
+
+import './style.css';
+import Navbar from "./components/Navbar"
+import { Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      
+        <Navbar />
+        
+    </div>
+  );
+}
+
+export default App;
