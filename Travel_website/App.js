d03845f5693c229d8a5450a23c64e2af32ce862a
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-04-09 20:53:44 +0300

    Update App.js

--- Travel_website/App.js
+++ Travel_website/App.js
@@ -1,13 +1,22 @@
 
 import './style.css';
 import Navbar from "./components/Navbar"
-import { Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import Home from "../src/components/routes/Home"
+import About from "../src/components/routes/About"
+import Service from "../src/components/routes/Service"
+import Contact from "../src/components/routes/Contact"
 
 
 function App() {
   return (
     <div className="App">
-      
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/service" element={<Service/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+        </Routes>
         <Navbar />
         
     </div>
