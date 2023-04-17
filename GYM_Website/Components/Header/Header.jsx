500944305e5ed2721078999aa6f5f10f205ac47d
Boyana Dimitrova <125132910+boyanaaaa@users.noreply.github.com>
2023-04-17 17:18:32 +0300

    Create Header.jsx

---
+++ GYM_Website/Components/Header/Header.jsx
@@ -0,0 +1,68 @@
+import React from 'react'
+import './Hero.css'
+import Header from '../Header/Header.jsx';
+import hero_image from "../../assets/www.png";
+import heart from "../../assets/heartb.png";
+
+const Hero = () => {
+    return(
+        <div className="hero">
+            <div className="left-hero">
+                <Header />
+
+                <div className="the-best-ad">
+                    <div></div>
+                    <span>the best fitness club in the town</span>
+                </div>
+
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal body</span>
+                    </div>
+                    <div>
+                        <span>In here we will help you to shape and build your ideal body and live up your life to fullest.</span>
+                    </div>
+                </div>
+                <div className='figures'>
+                    <div>
+                        <span>+140</span>
+                        <span>expert coaches</span>
+                    </div>
+
+                    <div>
+                        <span>+978</span>
+                        <span>members joined</span>
+                    </div>
+
+                    <div>
+                        <span>+50</span>
+                        <span>fitness programs</span>
+                    </div>
+                </div>
+
+
+                <div className="hero-buttons">
+                    <button className='btn'>Get started</button>
+                    <button className='btn'>Learn More</button>
+                </div>
+
+            </div>
+            <div className="right-hero">
+                <button className="btn">Join Now</button>
+
+                <div className="heart-rate">
+                    <img src={heart} />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
